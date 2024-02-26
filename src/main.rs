570b23c1bff96c80ca42861a869a07025c6eef5b
saltyfishie-opensuse <teugene888@gmail.com>
2024-02-27 00:13:27 +0800

    reduce poll_subscription

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,9 @@ use serde_json::json;
 use std::{process, time::Duration};
 
 async fn poll_subscription(
-    mut strm: mqtt::AsyncReceiver<Option<mqtt::Message>>,
+    strm: &mut mqtt::AsyncReceiver<Option<mqtt::Message>>,
 ) -> Option<mqtt::Message> {
-    match strm.next().await {
-        Some(msg_opt) => msg_opt,
-        None => None,
-    }
+    strm.next().await?
 }
 
 #[tokio::main]
@@ -37,6 +34,7 @@ async fn main() -> Result<(), mqtt::Error> {
             "[LWT] Async subscriber v5 lost connection",
             mqtt::QOS_1,
         );
+
         mqtt::ConnectOptionsBuilder::with_mqtt_version(MQTT_VERSION_5)
             .keep_alive_interval(Duration::from_millis(5000))
             .clean_start(false)
@@ -45,36 +43,47 @@ async fn main() -> Result<(), mqtt::Error> {
             .finalize()
     };
 
-    let strm = client.get_stream(25);
-    client.connect(conn_opts).await?;
+    let mut msg_stream = client.get_stream(25);
+
+    while let Err(e) = client.connect(conn_opts.clone()).await {
+        eprintln!("{}", e);
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
 
     let topic = "saltyfishie";
-    client.subscribe(topic, 1).await?;
-    println!("Subscribed to topic: {}", topic);
+    println!("Subscribing to topic: {}", topic);
+
+    while (tokio::time::timeout(Duration::from_secs(1), client.subscribe(topic, 1)).await).is_err()
+    {
+        eprintln!("Topic subsciption timeout!");
+        tokio::time::sleep(Duration::from_secs(2)).await;
+    }
+
     println!("Waiting for messages...");
 
     loop {
-        if let Some(msg) = poll_subscription(strm.clone()).await {
+        if let Some(msg) = poll_subscription(&mut msg_stream).await {
             if msg.retained() {
                 print!("(R) ");
             }
-            println!("{}", msg);
+            println!("\n{}", msg);
 
             let json = serde_json::from_slice::<serde_json::Value>(msg.payload()).unwrap();
 
             if let serde_json::Value::Object(data) = json {
-                let keys: Vec<String> = data.iter().map(|(k, _)| k.clone()).collect();
-                let values: Vec<serde_json::Value> = data.iter().map(|(_, v)| v.clone()).collect();
+                let mut keys = Vec::<&String>::new();
+                let mut values = Vec::<&serde_json::Value>::new();
 
-                let mut out = json!({
-                    "keys": keys,
-                    "values": values,
+                data.iter().for_each(|(k, v)| {
+                    keys.push(k);
+                    values.push(v)
                 });
 
-                out.as_object_mut().unwrap().insert(
-                    "timestamp".into(),
-                    serde_json::Value::String(chrono::Utc::now().to_string()),
-                );
+                let out = json!({
+                    "keys": keys,
+                    "values": values,
+                    "timestamp": chrono::Utc::now().to_string()
+                });
 
                 client.publish(mqtt::Message::new(
                     "saltyfishie/echo",
